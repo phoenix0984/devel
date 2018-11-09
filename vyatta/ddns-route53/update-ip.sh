7c1f2656e4c0a2733be5d162a539b679df33b989
Marcel Gisselmann <mg@affito.de>
2018-11-09 23:57:33 +0100

    Code cleanup; more pedantic use of variables

--- vyatta/ddns-route53/update-ip.sh
+++ vyatta/ddns-route53/update-ip.sh
@@ -25,8 +25,7 @@ key_secret="changeme"
 
 # ======================================================================= #
 
-if [ $(dpkg-query -W -f='${Status}' dnsutils 2>/dev/null | grep -c "ok installed") -eq 0 ]
-then
+if [[ $(dpkg-query -W -f='${Status}' dnsutils 2>/dev/null | grep -c "ok installed") -eq 0 ]]; then
   logger -s "Package dnsutils not installed. Quitting."
   exit 0
 fi
@@ -42,19 +41,33 @@ current_ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
 fqdn="${host}.${domain}."
 
 # Use authoritative nameserver from SOA RR
-dns_ns=$(dig +short SOA "$domain" | awk '{print $1}')
+dns_ns=$(dig +short SOA "${domain}" | awk '{print $1}')
 
 # Get the actual RRset from NS
-rrset=$(dig +nocmd +noall +answer @${dns_ns} ${fqdn})
-rrset_ttl=$(awk '{print $2}' <<< "$rrset")
-rrset_ip=$(awk '{print $5}' <<< "$rrset")
+rrset_noall=$(dig +nocmd +noall +answer @"${dns_ns}" "${fqdn}")
+rrset=$(dig +norecurse @"${dns_ns}" "${fqdn}")
+rrset_ttl=$(awk '{print $2}' <<< "${rrset_noall}")
+rrset_ip=$(awk '{print $5}' <<< "${rrset_noall}")
 
 # Check if either one of IP or TTL changed:
-if [ "$current_ip" = "$rrset_ip" ] && [ "$ttl" = "$rrset_ttl" ]
-  then
-  logger -s "Old IP "$rrset_ip" and TTL "$rrset_ttl" did not change. Quitting."
+if [[ "${current_ip}" = "${rrset_ip}" ]] && [[ "${ttl}" = "${rrset_ttl}" ]]; then
+  logger -s "Old IP "${rrset_ip}" and TTL "${rrset_ttl}" did not change. Quitting."
   exit 0
 fi
+
+# ======================================================================= #
+
+smtp_server="changeme"
+smtp_port="changeme"
+use_starttls="changeme"
+from_addr="changeme"
+from_pass="changeme"
+to_addr="changeme"
+cc_addr="changeme"
+msg_subj="Update received: "${fqdn}" updated to "${current_ip}" TTL: "${ttl}""
+msg_body="${rrset}"
+
+# ======================================================================= #
 
 update_cmd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )/route53-ddns.py"
 
@@ -67,5 +80,14 @@ ${update_cmd} \
   --ttl="${ttl}"\
   --dns_ttl="${rrset_ttl}"\
   --dns_ip="${rrset_ip}"\
+  --smtp_server="${smtp_server}"\
+  --smtp_port="${smtp_port}"\
+  --starttls="${use_starttls}"\
+  --from_addr="${from_addr}"\
+  --from_pass="${from_pass}"\
+  --to_addr="${to_addr}"\
+  --cc_addr="${cc_addr}"\
+  --msg_subj="${msg_subj}"\
+  --msg_body="${msg_body}"\
   --syslog
 exit $?
